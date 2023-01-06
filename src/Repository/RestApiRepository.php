a9c81bc657f3247b658154df24618f8d517722ba
Powernic <powernic1@gmail.com>
2023-01-06 20:24:20 +0300

    Add autorefresh token for rest api

--- src/Repository/RestApiRepository.php
+++ src/Repository/RestApiRepository.php
@@ -3,36 +3,48 @@
 namespace Camera\Repository;
 
 use Camera\Stream;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class RestApiRepository extends StreamRepository
 {
     private ?string $token = null;
+    private ?string $refreshToken = null;
+    private Client $client;
 
-    public function __construct(
-        private string $streamEndpoint = 'http://http/api/streams',
-        private string $authEndpoint = 'http://http/api/auth')
+    public function __construct()
     {
+        $this->client = new Client([
+            'base_uri' => 'http://http/api/',
+        ]);
     }
 
     private function getToken(): string
     {
         if ($this->token === null) {
-            $opts = [
-                'http' => [
-                    'method' => 'POST',
-                    'header' => 'Content-type: application/json',
-                    'content' => json_encode([
-                        "username" => $_ENV['API_USERNAME'],
-                        "password" => $_ENV['API_PASSWORD']
-                    ])
+            $response = $this->client->post('auth', [
+                'json' => [
+                    'username' => $_ENV['API_USERNAME'],
+                    "password" => $_ENV['API_PASSWORD']
                 ]
-            ];
-            $context = stream_context_create($opts);
-            $auth = @file_get_contents($this->authEndpoint, false, $context);
-            $response = json_decode($auth, true);
-            $this->token = $response['token'];
+            ]);
+            $data = json_decode($response->getBody()->getContents(), true);
+            $this->token = $data['token'];
+            $this->refreshToken = $data['refresh_token'];
         }
         return $this->token;
+    }
+
+    private function updateToken(): void
+    {
+        $response = $this->client->post('auth/refresh', [
+            'json' => [
+                'refresh_token' => $this->refreshToken,
+            ]
+        ]);
+        $data = json_decode($response->getBody()->getContents(), true);
+        $this->token = $data['token'];
+        $this->refreshToken = $data['refresh_token'];
     }
 
     /**
@@ -41,19 +53,26 @@ class RestApiRepository extends StreamRepository
      */
     public function all(): array
     {
-        $opts = [
-            'http' => [
-                'method' => 'GET',
-                'header' => 'Authorization: Bearer ' . $this->getToken()
-            ]
-        ];
-        $context = stream_context_create($opts);
-        $streamBody = @file_get_contents($this->streamEndpoint, false, $context);
-        if (!empty($streamBody)) {
-            $streamConfig = json_decode($streamBody, true);
-        } else {
-            throw new \Exception('Could not get stream config from API');
+        try {
+            $response = $this->client->get('streams', [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . $this->getToken(),
+                ]
+            ]);
+        } catch (ClientException $e) {
+            if ($e->getCode() !== 401)
+                throw $e;
+            $content = $e->getResponse()->getBody()->getContents();
+            $error = json_decode($content, true);
+            if ($error['message'] !== 'Expired JWT Token') {
+                throw new \Exception($error['message']);
+            }
+            $this->updateToken();
+            return $this->all();
         }
+        $streamBody = $response->getBody()->getContents();
+        $streamConfig = json_decode($streamBody, true);
         return array_map(function ($streamData) {
             return new Stream($streamData['id'], $streamData['url']);
         }, $streamConfig);
